4d2db8a679ae963f3fd8197ed40ab304d8f1adfd
GACHET Vivien <gachet.vivien@gmail.com>
2026-09-10 14:16:52 +0200

    changed path to folder

--- lftp_script.sh
+++ lftp_script.sh
@@ -1,4 +1,4 @@
 set sftp:auto-confirm yes
 open -p 22 -u vgachet sftp://perso.ens-lyon.fr
-mirror -e  --no-umask -R /home/vgachet/Documents/site_pro/public/ ~
+mirror -e  --no-umask -R /home/vgachet/Documents/Professionel/site_pro/public/ ~
 close
